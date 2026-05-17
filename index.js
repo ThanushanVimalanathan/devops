2617194d1fbff30990f2838bacaf9c869ad08d3f
VimalanathanThanushan <thanushaan69@gmail.com>
2026-05-17 19:44:37 +0530

    feat:add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello devops!");
+console.log("This is a sample code for devops.");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log('Starting test script...');
+setTimeout(() => console.log('Test script completed.'), 3000);
+console.log('This is a test is completed'); 
